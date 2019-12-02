01e335dd407f99e66fb1fa692dccab37d7365e31
Dsm0 <willrinkoff@gmail.com>
2019-12-01 21:20:40 -0500

    removed old p5.hs folder

--- p5.hs/test/p5test.hs
+++
@@ -1,9 +0,0 @@
-import Test.HUnit
--- import P5Canvas.hs
-
-testBox = "hadljw"
-testRect = "hadljw"
-
-test1 = TestCase (assertEqual "rect w x y z == rect(w,x,y,z)" testRec "rect(w,x,y,z)")
-
-testList = TestList [test]
