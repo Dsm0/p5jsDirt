4873267bcb47a396f855095092a9f672d6a11414
Dsm0 <willrinkoff@gmail.com>
2021-06-13 18:18:54 -0500

    added audio back in

--- web/js/superDirt.js
+++ web/js/superDirt.js
@@ -1,53 +1,55 @@
 var messages = [new Thing({})];
 var drawFunc = "box(200,200,200)";
-var assets = {"images" : {}};
-let microphone, fft,level;
+var assets = { "images": {} };
+let microphone, fft, level;
 
 function setup() {
 
-  getAudioContext().suspend();
 
-  createCanvas(windowWidth, windowHeight,WEBGL);
+  createCanvas(windowWidth, windowHeight, WEBGL);
   textSize(40);
   rockSalt = loadFont("fonts/RockSalt.ttf");
-  textFont(rockSalt,48);
-  microphone = new p5.AudioIn();
-  microphone.start();
-  
+  textFont(rockSalt, 48);
+
   console.log("click screen once to enable audio-in");
 
-  // amplitude = new p5.Amplitude();
-  // level = amplitude.getLevel();
+  getAudioContext().suspend();
+
+  microphone = new p5.AudioIn();
+  microphone.start();
+
+  amplitude = new p5.Amplitude();
+  level = amplitude.getLevel();
 
   fft = new p5.FFT();
   fft.setInput(microphone);
 
-
 };
 
-function saveShader(shaderName,shaderVert,shaderFrag){
-  shaderAttributes = createShader(shaderVert,shaderFrag)
+function saveShader(shaderName, shaderVert, shaderFrag) {
+  shaderAttributes = createShader(shaderVert, shaderFrag)
   console.log("shader:" + shaderName + " saved to \"shaders\"");
   assets["shaders"][shaderName] = shader(shaderAttributes);
-  }
+}
 
-function saveImage(imageUrl,name){
-  img = createImg(imageUrl,"imgs/notFound.jpg","anonymous");
+function saveImage(imageUrl, name) {
+  img = createImg(imageUrl, "imgs/notFound.jpg", "anonymous");
   // console.log(img.crossOrigin);
   img.hide()
   console.log("IMG:");
   console.log(img);
-  img.crossOrigin="anonymous";
+  img.crossOrigin = "anonymous";
   assets["images"][name] = img;
-  }
+}
 
 
-function draw(){
+function draw() {
   background(0);
   var message = messages[messages.length - 1];
+  if(drawFunc === "") return;
   try {
     eval(drawFunc);
-  }catch(err) {
+  } catch (err) {
     text("some sort of error");
     // eval(lastDrawFunc);
     console.log("drawFunc");
@@ -67,18 +69,18 @@ function oscEvent(m) { // console.log("in osc event")
   var i;
   var paramDict = {};
 
-  for(i = 0; i < m.args.length; ++i) {
+  for (i = 0; i < m.args.length; ++i) {
     // console.log(m.args[i])
     var param = m.args[i];
-    var paramValue = m.args[i+1];
+    var paramValue = m.args[i + 1];
     paramDict[param] = paramValue;
     ++i; // it actually only looks at every other value, b/c there are 2 ++i's
   };
 
-  switch(paramDict['draw']) {
+  switch (paramDict['draw']) {
     case "":
-    drawFunc = "";
-    // console.log("things: " + things);
+      drawFunc = "";
+      // console.log("things: " + things);
       break;
     case undefined:
       break;
@@ -86,7 +88,7 @@ function oscEvent(m) { // console.log("in osc event")
       drawFunc = paramDict['draw'];
   };
 
-  switch(paramDict['setup']) {
+  switch (paramDict['setup']) {
     case "":
       break;
     case undefined:
@@ -95,7 +97,7 @@ function oscEvent(m) { // console.log("in osc event")
       setupCall(paramDict['setup']);
   };
 
-  switch(paramDict["shaderName"]){
+  switch (paramDict["shaderName"]) {
     case undefined:
       break;
     default:
@@ -104,11 +106,11 @@ function oscEvent(m) { // console.log("in osc event")
       shaderVert = paramDict["shaderVert"]
       shaderFrag = paramDict["shaderFrag"]
       console.log("loading shader: " + shaderName)
-      saveShader(shaderName,shaderVert,shaderFrag)
+      saveShader(shaderName, shaderVert, shaderFrag)
       console.log(assets)
   }
 
-  switch(paramDict['imageURL']) {
+  switch (paramDict['imageURL']) {
     case undefined:
       break;
     default:
@@ -116,22 +118,22 @@ function oscEvent(m) { // console.log("in osc event")
       // lastDrawFunc = "box(200,200,200)";
       imageURL = paramDict['imageURL'];
       imageName = paramDict['imageName'];
-      saveImage(imageURL,imageName)
+      saveImage(imageURL, imageName)
       console.log(assets)
 
   };
 
-  if(paramDict['s'] != undefined) {
+  if (paramDict['s'] != undefined) {
     messages.push(new Thing(paramDict));
   }
 }
 
-function mousePressed() {
-  userStartAudio();
-  console.log("audio enabled");
-}
+// function mousePressed() {
+//   userStartAudio();
+//   console.log("audio enabled");
+// }
 
-function setupCall(func){
+function setupCall(func) {
   eval(func);
   console.log("param change evaluated");
 }
